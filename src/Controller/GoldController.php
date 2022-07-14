ca7cf1c524b62ee048e8626ed100fbf153d40fe1
Krzysztof Łagan <krzysztof.lagan@softnauts.com>
2022-07-14 09:39:19 +0200

    Configured database for future use

--- src/Controller/GoldController.php
+++ src/Controller/GoldController.php
@@ -8,9 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GoldController extends AbstractController
 {
-    #[Route('/api/gold', name: 'app_gold', methods: [ 'POST' ])]
+    #[Route('/api/gold', name: 'app_gold')]
     public function index(): JsonResponse
     {
+
+        // Replace this placeholder with real code
+
         return $this->json([
             'from' => '2001-01-04T00:00:00+00:00',
             'to' => '2001-01-04T00:00:00+00:00',
